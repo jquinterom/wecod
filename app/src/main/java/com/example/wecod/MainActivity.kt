c7cfbb047ff05bae719f33bd1fde0ddfc141d4c9
jquinterom <jfqm7710@gmail.com>
2023-08-07 22:07:01 -0500

    First desing for weapon card

--- app/src/main/java/com/example/wecod/MainActivity.kt
+++ app/src/main/java/com/example/wecod/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,22 +24,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    for (i in 1..10){
+                        WeaponCard()
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    WeaponCard()
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     WeCodTheme {
-        Greeting("Android")
+        WeaponCard()
     }
 }
--- app/src/main/java/com/example/wecod/composables/WeaponCard.kt
+++ app/src/main/java/com/example/wecod/composables/WeaponCard.kt
@@ -1,20 +1,88 @@
 package com.example.wecod.composables
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import com.example.wecod.R
 import com.example.wecod.ui.theme.WeCodTheme
 
 @Composable
 fun WeaponCard() {
     Card(
-        modifier = Modifier
+        modifier = Modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
-        Text("card view")
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+
+            SubcomposeAsyncImage(
+                model = "https://i.ytimg.com/vi/xRp0l_qZi9Y/maxresdefault.jpg",
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable { }
+                    .fillMaxHeight()
+                    .fillMaxWidth(.4f),
+                alignment = Alignment.TopStart,
+            ) {
+                val state = painter.state
+                if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                    Box(
+                        modifier = Modifier.clickable { },
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                } else {
+                    SubcomposeAsyncImageContent()
+                }
+            }
+
+            DescriptionWeapon()
+            Stars()
+        }
+    }
+}
+
+@Composable
+fun DescriptionWeapon() {
+    Column() {
+        Text(text = "DQL")
+        Text(text = "Sniper")
+    }
+}
+
+@Composable
+fun Stars() {
+    Row {
+        for (i in 1..5) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_star_border_24),
+                contentDescription = null
+            )
+        }
     }
 }
 
