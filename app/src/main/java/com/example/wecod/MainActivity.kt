2fd9e1652b569c18aa485636eb67fd1c62b16238
jquinterom <jfqm7710@gmail.com>
2023-08-08 17:27:59 -0500

    Desing weapon card v1

--- app/src/main/java/com/example/wecod/MainActivity.kt
+++ app/src/main/java/com/example/wecod/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -24,8 +25,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    for (i in 1..10){
-                        WeaponCard()
+                    LazyColumn {
+                        items(10 ) {
+                            WeaponCard()
+                        }
                     }
                 }
             }
--- app/src/main/java/com/example/wecod/composables/WeaponCard.kt
+++ app/src/main/java/com/example/wecod/composables/WeaponCard.kt
@@ -1,13 +1,20 @@
 package com.example.wecod.composables
 
+import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -16,6 +23,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,67 +38,89 @@ import com.example.wecod.ui.theme.WeCodTheme
 @Composable
 fun WeaponCard() {
     Card(
-        modifier = Modifier,
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 4.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(10.dp)
     ) {
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
+        Column(
+            modifier = Modifier.fillMaxSize()
         ) {
-
-            SubcomposeAsyncImage(
-                model = "https://i.ytimg.com/vi/xRp0l_qZi9Y/maxresdefault.jpg",
-                contentDescription = null,
-                modifier = Modifier
-                    .clickable { }
-                    .fillMaxHeight()
-                    .fillMaxWidth(.4f),
-                alignment = Alignment.TopStart,
+            Row(
+                modifier = Modifier.padding(end = 4.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                val state = painter.state
-                if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
-                    Box(
-                        modifier = Modifier.clickable { },
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        CircularProgressIndicator()
+                SubcomposeAsyncImage(
+                    model = "https://i.ytimg.com/vi/xRp0l_qZi9Y/maxresdefault.jpg",
+                    contentDescription = null,
+                    modifier = Modifier
+                        .clickable { }
+                        .fillMaxWidth(.4f)
+                        .clip(RoundedCornerShape(5.dp)),
+                    alignment = Alignment.TopStart,
+                    contentScale = ContentScale.Crop,
+                ) {
+                    val state = painter.state
+                    if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                        Box(
+                            modifier = Modifier.clickable { },
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            CircularProgressIndicator()
+                        }
+                    } else {
+                        SubcomposeAsyncImageContent()
                     }
-                } else {
-                    SubcomposeAsyncImageContent()
                 }
-            }
 
-            DescriptionWeapon()
-            Stars()
+                Spacer(modifier = Modifier.width(8.dp))
+                DescriptionWeapon()
+            }
         }
     }
 }
 
 @Composable
 fun DescriptionWeapon() {
-    Column() {
+    Column(
+        modifier = Modifier.fillMaxHeight(),
+        horizontalAlignment = Alignment.Start
+    ) {
         Text(text = "DQL")
+        Spacer(modifier = Modifier.width(4.dp))
         Text(text = "Sniper")
+
+        Stars()
     }
 }
 
 @Composable
 fun Stars() {
-    Row {
-        for (i in 1..5) {
-            Icon(
-                painter = painterResource(id = R.drawable.baseline_star_border_24),
-                contentDescription = null
-            )
+    Row(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.End,
+    ) {
+        Row {
+            for (i in 1..5) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_star_border_24),
+                    contentDescription = null
+                )
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
-fun WeaponCardPreview() {
+private fun WeaponCardPreview() {
     WeCodTheme {
         WeaponCard()
     }
